e7ae5b39ac7ef7490004c571854369cc465b7302
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-11-29 12:00:04 -0500

    Número de Municipios Afectados

--- covid.py
+++ covid.py
@@ -14,3 +14,18 @@ data = pd.read_csv(url)
 # 1. Numero de casos de contagiados en el pais
 n_contagios = data.shape[0]
 print(f'\nCasos de Covid-19 en Colombia: {n_contagios}')
+
+
+# 2. Número de Municipios Afectados
+data['Nombre municipio'].replace('puerto COLOMBIA', 'PUERTO COLOMBIA', inplace=True)
+data['Nombre municipio'].replace('puerto colombia', 'PUERTO COLOMBIA', inplace=True)
+data['Nombre municipio'].replace('barrancabermeja', 'BARRANCABERMEJA', inplace=True)
+data['Nombre municipio'].replace('momil', 'MOMIL', inplace=True)
+data['Nombre municipio'].replace('MEDELLiN', 'MEDELLIN', inplace=True)
+data['Nombre municipio'].replace('Galapa', 'GALAPA', inplace=True)
+data['Nombre municipio'].replace('Guepsa', 'GUEPSA', inplace=True)
+data['Nombre municipio'].replace('Pensilvania', 'PENSILVANIA', inplace=True)
+data['Nombre municipio'].replace('Anserma', 'ANSERMA', inplace=True)
+
+n_municipios = len(data.groupby('Nombre municipio').size())
+print(f'\nNumero de municipios afectados: {n_municipios}')
