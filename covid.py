636c5ae171083f3aebdc86729b8eee26b2cdc68e
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-11-29 11:52:42 -0500

    Commit Inicial

---
+++ covid.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 11:50:48 2021
+
+@author: ivandavid
+"""
+import pandas as pd
+
+url = "covid_22_noviembre.csv"
+data = pd.read_csv(url)
